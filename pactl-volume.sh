2c53d6e2c3813ce7b552b871bcc163d106b4d50b
Kimmo Tapala <kimmo.tapala@karhuhelsinki.fi>
2023-12-06 14:04:40 +0200

    Switch audio volume icon based on mute status

--- pactl-volume.sh
+++ pactl-volume.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/sh
 
-pactl get-sink-volume @DEFAULT_SINK@ | awk '/Volume:/ {print "{\"text\": \"🔈" $5 "\"}"}' | sed 's/%//g'
+ICON=$(pactl get-sink-mute @DEFAULT_SINK@ | awk '/Mute:/ {print $2}' | sed 's/yes/🔇/g' | sed 's/no/🔊/g')
+pactl get-sink-volume @DEFAULT_SINK@ | awk '/Volume:/ {print "{\"text\": \"ICON" $5 "\"}"}' | sed 's/%//g' | sed "s/ICON/$ICON/g"
